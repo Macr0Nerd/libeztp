3088e02a8a860e469b811881c8001dd389645ea5
Macr0Nerd <macronerdart@gmail.com>
2020-04-18 16:33:41 -0500

    Added single include header

---
+++ include/eztp.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef EZTPLIB_EZTP_HPP
+#define EZTPLIB_EZTP_HPP
+
+#include "Core/dice.hpp"
+
+#include "Character/armor.hpp"
+#include "Character/weapons.hpp"
+#include "Character/race.hpp"
+#include "Character/background.hpp"
+#include "Character/character.hpp"
+
+#endif //EZTPLIB_EZTP_HPP
