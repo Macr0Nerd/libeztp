c2e9a2053ff42f0e1dd66207c86631330a90ee75
Macr0Nerd <macronerdart@gmail.com>
2020-04-20 23:42:15 -0500

    Race tests...

--- test/baseTests.cpp
+++ test/baseTests.cpp
@@ -11,27 +11,27 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
 
         SECTION("d0") {
             INFO("d0 Test Start");
-            std::cout << "Starting d0 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d0 Tests..." << std::endl;
 
             x = eztp::d0.roll();
             REQUIRE(x == 0);
 
-            std::cout << "Passed d0" << std::endl << std::flush;
+            std::cout << "Passed d0" << std::endl;
         }
 
         SECTION("d1") {
             INFO("d1 Test Start");
-            std::cout << "Starting d1 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d1 Tests..." << std::endl;
 
             x = eztp::d1.roll();
             REQUIRE(x == 1);
 
-            std::cout << "Passed d1" << std::endl << std::flush;
+            std::cout << "Passed d1" << std::endl;
         }
 
         SECTION("d2") {
             INFO("d2 Test Start");
-            std::cout << "Starting d2 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d2 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d2 test " << i) {
@@ -43,12 +43,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d2" << std::endl << std::flush;
+            std::cout << "Passed d2" << std::endl;
         }
 
         SECTION("d4") {
             INFO("d4 Test Start");
-            std::cout << "Starting d4 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d4 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d4 test " << i) {
@@ -60,12 +60,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d4" << std::endl << std::flush;
+            std::cout << "Passed d4" << std::endl;
         }
 
         SECTION("d6") {
             INFO("d6 Test Start");
-            std::cout << "Starting d6 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d6 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d6 test " << i) {
@@ -77,12 +77,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d6" << std::endl << std::flush;
+            std::cout << "Passed d6" << std::endl;
         }
 
         SECTION("d8") {
             INFO("d8 Test Start");
-            std::cout << "Starting d8 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d8 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d8 test" << i) {
@@ -94,12 +94,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d8" << std::endl << std::flush;
+            std::cout << "Passed d8" << std::endl;
         }
 
         SECTION("d10") {
             INFO("d10 Test Start");
-            std::cout << "Starting d10 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d10 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d10 test" << i) {
@@ -111,12 +111,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d10" << std::endl << std::flush;
+            std::cout << "Passed d10" << std::endl;
         }
 
         SECTION("d12") {
             INFO("d12 Test Start");
-            std::cout << "Starting d12 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d12 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d12 test" << i) {
@@ -128,12 +128,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d12" << std::endl << std::flush;
+            std::cout << "Passed d12" << std::endl;
         }
 
         SECTION("d20") {
             INFO("d20 Test Start");
-            std::cout << "Starting d20 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d20 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d20 test" << i) {
@@ -145,12 +145,12 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d20" << std::endl << std::flush;
+            std::cout << "Passed d20" << std::endl;
         }
 
         SECTION("d100") {
             INFO("d100 Test Start");
-            std::cout << "Starting d100 Tests..." << std::endl << std::flush;
+            std::cout << "Starting d100 Tests..." << std::endl;
 
             for (int i = 0; i < 5; ++i) {
                 DYNAMIC_SECTION("d100 test" << i) {
@@ -162,10 +162,10 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed d100" << std::endl << std::flush;
+            std::cout << "Passed d100" << std::endl;
         }
 
-        std::cout << "Passed Default Dice" << std::endl << std::flush;
+        std::cout << "Passed Default Dice" << std::endl;
     }
 
     SECTION("Custom dice") {
@@ -173,18 +173,18 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
 
         SECTION("Default Case") {
             INFO("Default Test Start");
-            std::cout << "Starting default case Tests..." << std::endl << std::flush;
+            std::cout << "Starting default case Tests..." << std::endl;
 
             REQUIRE(die.roll() == -1);
 
-            std::cout << "Passed default case" << std::endl << std::flush;
+            std::cout << "Passed default case" << std::endl;
         }
 
         SECTION("Custom Case") {
             int x;
 
             INFO("Custom Case Start");
-            std::cout << "Starting custom case Tests..." << std::endl << std::flush;
+            std::cout << "Starting custom case Tests..." << std::endl;
 
             die = eztp::core::dice(50);
 
@@ -199,64 +199,64 @@ TEST_CASE("Dice can roll within a range", "[dice]") {
                 }
             }
 
-            std::cout << "Passed Custom Case" << std::endl << std::flush;
+            std::cout << "Passed Custom Case" << std::endl;
         }
 
         SECTION("Non-Natural Case") {
             die = eztp::core::dice(-10);
 
             INFO("Non-natural Test Start");
-            std::cout << "Starting non-natural case Tests..." << std::endl << std::flush;
+            std::cout << "Starting non-natural case Tests..." << std::endl;
 
             CHECK(die.roll() == -10);
 
-            std::cout << "Passed non-natural case" << std::endl << std::flush;
+            std::cout << "Passed non-natural case" << std::endl;
         }
 
-        std::cout << "Passed Custom Dice" << std::endl << std::flush;
+        std::cout << "Passed Custom Dice" << std::endl;
     }
 }
 
 TEST_CASE("Weapons data is accessible", "[weapons]") {
     SECTION("Dice check") {
         INFO("Dice tests starting");
-        std::cout << "Starting weapons/dice Tests..." << std::endl << std::flush;
+        std::cout << "Starting weapons/dice Tests..." << std::endl;
 
         CHECK(eztp::character::weapons::weaps["DAGGER"].die == eztp::d4);
         CHECK(eztp::character::weapons::weaps["SHORTBOW"].die == eztp::d6);
         CHECK(eztp::character::weapons::weaps["LONGSWORD+"].die == eztp::d10);
         CHECK(eztp::character::weapons::weaps["LONGBOW"].die == eztp::d8);
 
-        std::cout << "Passed dice tests" << std::endl << std::flush;
+        std::cout << "Passed dice tests" << std::endl;
     }
 
     SECTION("Ability check") {
         INFO("Ability tests starting");
-        std::cout << "Starting weapons/ability Tests..." << std::endl << std::flush;
+        std::cout << "Starting weapons/ability Tests..." << std::endl;
 
         CHECK(eztp::character::weapons::weaps["DAGGER"].ability == 3);
         CHECK(eztp::character::weapons::weaps["SHORTBOW"].ability == 2);
         CHECK(eztp::character::weapons::weaps["LONGSWORD+"].ability == 1);
         CHECK(eztp::character::weapons::weaps["LONGBOW"].ability == 2);
 
-        std::cout << "Passed ability tests" << std::endl << std::flush;
+        std::cout << "Passed ability tests" << std::endl;
     }
 
     SECTION("Martial Check") {
         INFO("Martial tests starting");
-        std::cout << "Starting weapons/martial Tests..." << std::endl << std::flush;
+        std::cout << "Starting weapons/martial Tests..." << std::endl;
 
         CHECK_FALSE(eztp::character::weapons::weaps["DAGGER"].martial);
         CHECK_FALSE(eztp::character::weapons::weaps["SHORTBOW"].martial);
         CHECK(eztp::character::weapons::weaps["LONGSWORD+"].martial);
         CHECK(eztp::character::weapons::weaps["LONGBOW"].martial);
 
-        std::cout << "Passed martial tests" << std::endl << std::flush;
+        std::cout << "Passed martial tests" << std::endl;
     }
 
     SECTION("Range Check") {
         INFO("Range tests starting");
-        std::cout << "Starting weapons/range Tests..." << std::endl << std::flush;
+        std::cout << "Starting weapons/range Tests..." << std::endl;
 
         CHECK(eztp::character::weapons::weaps["DAGGER"].ranged == 2);
         CHECK(eztp::character::weapons::weaps["DAGGER"].range.first == 20);
@@ -274,42 +274,81 @@ TEST_CASE("Weapons data is accessible", "[weapons]") {
         CHECK(eztp::character::weapons::weaps["NET"].range.first == 5);
         CHECK(eztp::character::weapons::weaps["NET"].range.second == 15);
 
-        std::cout << "Passed range tests" << std::endl << std::flush;
+        std::cout << "Passed range tests" << std::endl;
     }
 }
 
 TEST_CASE("Armor data is accessible", "[armor]") {
     SECTION("Base AC") {
         INFO("Base AC Tests");
-        std::cout << "Starting armor/ac Tests..." << std::endl << std::flush;
+        std::cout << "Starting armor/ac Tests..." << std::endl;
 
         CHECK(eztp::character::armor::armors["LEATHER"].baseAC == 11);
         CHECK(eztp::character::armor::armors["BREASTPLATE"].baseAC == 14);
         CHECK(eztp::character::armor::armors["PLATE"].baseAC == 18);
 
-        std::cout << "Passed base AC" << std::endl << std::flush;
+        std::cout << "Passed base AC" << std::endl;
     }
 
     SECTION("Armor type") {
         INFO("Armor Type Tests");
-        std::cout << "Starting armor/type Tests..." << std::endl << std::flush;
+        std::cout << "Starting armor/type Tests..." << std::endl;
 
         CHECK(eztp::character::armor::armors["LEATHER"].armType == 'L');
         CHECK(eztp::character::armor::armors["BREASTPLATE"].armType == 'M');
         CHECK(eztp::character::armor::armors["PLATE"].armType == 'H');
 
-        std::cout << "Passed armor type" << std::endl << std::flush;
+        std::cout << "Passed armor type" << std::endl;
     }
 
     SECTION("Stealth") {
         INFO("Stealth Tests");
-        std::cout << "Starting armor/stealth Tests..." << std::endl << std::flush;
+        std::cout << "Starting armor/stealth Tests..." << std::endl;
 
         CHECK_FALSE(eztp::character::armor::armors["LEATHER"].disadvantage);
         CHECK_FALSE(eztp::character::armor::armors["BREASTPLATE"].disadvantage);
         CHECK(eztp::character::armor::armors["PLATE"].disadvantage);
 
-        std::cout << "Passes stealth tests" << std::endl << std::flush;
+        std::cout << "Passes stealth tests" << std::endl;
     }
 }
 
+TEST_CASE("Race data is accessible", "[races") {
+    SECTION("Abilities/Boosts") {
+        INFO("Abilities/Boosts Tests");
+        std::cout << "Starting abilities/boosts Tests..." << std::endl;
+
+        CHECK(eztp::character::race::races["HILL DWARF"].abis[1] == "WIS");
+        CHECK(eztp::character::race::races["HILL DWARF"].boosts[1] == 1);
+
+        CHECK(eztp::character::race::races["HIGH ELF"].abis[0] == "DEX");
+        CHECK(eztp::character::race::races["HIGH ELF"].boosts[0] == 2);
+
+        CHECK(eztp::character::race::races["TIEFLING"].abis[1] == "CHA");
+        CHECK(eztp::character::race::races["TIEFLING"].boosts[1] == 2);
+
+        std::cout << "Passed abilities/boosts tests" << std::endl;
+    }
+
+    SECTION("Size") {
+        INFO("Size Tests");
+        std::cout << "Starting size Tests..." << std::endl;
+
+        CHECK(eztp::character::race::races["HALF-ORC"].size == 'M');
+        CHECK(eztp::character::race::races["LIGHTFOOT HALFLING"].size == 'S');
+        CHECK(eztp::character::race::races["HUMAN"].size == 'M');
+
+        std::cout << "Passed size tests" << std::endl;
+    }
+
+    SECTION("Speed") {
+        INFO("Speed Tests");
+        std::cout << "Starting speed Tests..." << std::endl;
+
+        CHECK(eztp::character::race::races["HALF-ELF"].speed == 30);
+        CHECK(eztp::character::race::races["HILL DWARF"].speed == 25);
+        CHECK(eztp::character::race::races["HUMAN"].speed == 30);
+
+        std::cout << "Passed speed tests" << std::endl;
+    }
+}
